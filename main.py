86a6ea8d97dfc0b37e9754699129013a250f529c
Markus Möller <markus.moeller@outlook.de>
2024-02-21 23:43:46 +0100

    refactor: main to contain current course part and gitignore to ignore pycache relatet files

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-from preprocessors import tools
+from ml_models import regression
+from preprocessors import tools, plotter
 
 if __name__ == "__main__":
-    # Prepare the dataset
-    data_set = "data/Data.csv"
-    dataset, analyse_data = tools.import_csv_dataset(data_set)
-    feature_matrix = dataset.iloc[:, :-1].values
-    dependent_vector = dataset.iloc[:, -1].values
+    file = "data/Salary_Data.csv"
+    df, inspection = tools.import_csv_dataset(file)
 
-    # Missing numerical data
-    refined_feature_matrix = tools.missing_numerical_data(feature_matrix)
+    matrix = df.iloc[:, :-1].values
+    dependent_variable = df.iloc[:, -1].values
 
-    # Encoding categorical data
-    encoded_feature_matrix = tools.encoding_categorical_data(refined_feature_matrix)
+    matrix_train, matrix_test, vector_train, vector_test = tools.split_dataset(
+        matrix, dependent_variable, test_size=1 / 3, random_state=0
+    )
 
-    # Encoding dependent vector
-    encoded_dependent_vector = tools.encode_dependent_vector(dependent_vector)
+    test_pred, train_pred = regression.simple_linear_regression(matrix_train, vector_train, matrix_test)
 
-    # Splitting the dataset into the Training set and Test set
-    train_matrix, test_matrix, train_vector, test_vector = tools.split_dataset(
-        encoded_feature_matrix, encoded_dependent_vector)
-    
-    # Feature scaling
-    train_matrix, test_matrix = tools.feature_scaling(test_matrix, train_matrix)
+    train_plt = plotter.scatter_plot(matrix_train, vector_train, train_pred, {
+        "sc_color": "orange",
+        "plt_color": "green",
+        "title": "Salary vs Experience (Training set)",
+        "x_label": "Years of Experience",
+        "y_label": "Salary"
+    })
+
+    test_plt = plotter.scatter_plot(matrix_test, vector_test, test_pred, {
+        "sc_color": "red",
+        "plt_color": "blue",
+        "title": "Salary vs Experience (Test set)",
+        "x_label": "Years of Experience",
+        "y_label": "Salary"
+    })
