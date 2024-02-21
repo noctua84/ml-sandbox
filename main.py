fd9a66165881ea5d390fd0b2013cd230f2ea5461
Markus Möller <markus.moeller@outlook.de>
2024-02-21 22:14:40 +0100

    preprocession tooling finished

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from preprocessors import tools
 
 if __name__ == "__main__":
+    # Prepare the dataset
     data_set = "data/Data.csv"
-    raw_x, raw_y = tools.import_csv_dataset(data_set)
+    dataset, analyse_data = tools.import_csv_dataset(data_set)
+    feature_matrix = dataset.iloc[:, :-1].values
+    dependent_vector = dataset.iloc[:, -1].values
 
-    refined_x = tools.missing_data(raw_x)
-    print(refined_x)
+    # Missing numerical data
+    refined_feature_matrix = tools.missing_numerical_data(feature_matrix)
+
+    # Encoding categorical data
+    encoded_feature_matrix = tools.encoding_categorical_data(refined_feature_matrix)
+
+    # Encoding dependent vector
+    encoded_dependent_vector = tools.encode_dependent_vector(dependent_vector)
+
+    # Splitting the dataset into the Training set and Test set
+    train_matrix, test_matrix, train_vector, test_vector = tools.split_dataset(
+        encoded_feature_matrix, encoded_dependent_vector)
+    
+    # Feature scaling
+    train_matrix, test_matrix = tools.feature_scaling(test_matrix, train_matrix)
--- preprocessors/tools.py
+++ preprocessors/tools.py
@@ -1,21 +1,42 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
 
 
 def import_csv_dataset(path: str):
     """Import a dataset from a CSV file"""
-    dataset = pd.read_csv(path)
-    feature_matrix = dataset.iloc[:, :-1].values
-    dependent_vector = dataset.iloc[:, -1].values
+    def delimiter_check():
+        """Check the delimiter of the CSV file"""
+        with open(path, 'r') as file:
+            first_line = file.readline()
+            if first_line.count(",") > first_line.count(";"):
+                return ","
+            else:
+                return ";"
 
-    return feature_matrix, dependent_vector
+    dataset = pd.read_csv(path, delimiter=delimiter_check())
+
+    return dataset, analyse_dataset(dataset)
 
 
-def missing_data(matrix: np.array):
+def analyse_dataset(dataset: pd.DataFrame):
+    """Analyse the dataset"""
+
+    missing_count = dataset.isnull().sum()
+    available_columns = dataset.columns
+    cols_with_missing_data = dataset.columns[dataset.isnull().any()]
+
+    return {
+        "missing_count": missing_count,
+        "available_columns": available_columns,
+        "missing_data": cols_with_missing_data
+    }
+
+
+def missing_numerical_data(matrix: np.array):
     """Replace missing data with the mean of the column"""
     def is_numerical(cur_col):
         """Check if the column is numerical"""
@@ -34,3 +55,43 @@ def missing_data(matrix: np.array):
             matrix[:, col:col+1] = imputer.transform(matrix[:, col:col+1])
 
     return matrix
+
+
+def encoding_categorical_data(matrix: np.array):
+    """Encode categorical data"""
+    def is_categorical(cur_col):
+        """Check if the column is categorical"""
+        try:
+            cur_col.astype(float)
+            return False
+        except ValueError:
+            return True
+
+    categorical_cols = [i for i in range(matrix.shape[1]) if is_categorical(matrix[:, i])]
+
+    if categorical_cols:
+        transformer = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), categorical_cols)],
+                                        remainder='passthrough')
+        matrix = transformer.fit_transform(matrix)
+
+    return matrix
+
+
+def encode_dependent_vector(vector: np.array):
+    """Encode the dependent vector"""
+    encoder = LabelEncoder()
+    return encoder.fit_transform(vector)
+
+
+def split_dataset(x, y, test_size=0.2, random_state=1):
+    """Split the dataset into training and test sets"""
+    return train_test_split(x, y, test_size=test_size, random_state=random_state)
+
+
+def feature_scaling(x_test, x_train):
+    """Feature scaling"""
+    sc = StandardScaler()
+    x_train = sc.fit_transform(x_train)
+    x_test = sc.transform(x_test)
+
+    return x_test, x_train
